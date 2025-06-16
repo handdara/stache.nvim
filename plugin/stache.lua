40e0acb81fda62148cd98d8dfd3203769f286f37
handdara <handdara.core@gmail.com>
2025-06-16 11:59:14 -0400

    Fixed startup error

--- plugin/stache.lua
+++ plugin/stache.lua
@@ -4,11 +4,3 @@ end, {})
 vim.api.nvim_create_user_command('StacheRunAll', function(_)
     require 'stache'.buf_exec_all_blocks(0)
 end, {})
-local stache_enter = vim.api.nvim_create_augroup('StacheEnter', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-    callback = function()
-        vim.bo.filetype = "yaml"
-    end,
-    group = stache_enter,
-    pattern = require 'stache'.options.dirs.data .. '/*',
-})
